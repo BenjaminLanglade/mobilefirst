ccd132d62e3070547328967c46b790eadefb422a
BenjaminLanglade <langlade89@gmail.com>
2016-07-06 22:50:44 +0200

    Final Project

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+
+var source = './app/src';
+var prod = './app/prod';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -7,36 +11,33 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var cleanCSS = require('gulp-clean-css');
 
-var source = './app/src';
-var prod = './app/prod';
-
 gulp.task('style', function() {
   return gulp.src(source + '/sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest(prod + '/css/'))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest(prod + '/css/'))
 });
 
 gulp.task('image', function () {
   return gulp.src(source + '/images/*.{png,jpg,jpeg,gif,svg}')
-    .pipe(imagemin())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(prod + '/images'))
+  .pipe(imagemin())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest(prod + '/images'))
 });
 
 gulp.task('script', function() {
   return gulp.src(source + '/scripts/*.js')
-    .pipe(concat('script.min.js'))
-    .pipe(uglify().on('error', function(e){
-        console.log(e);
-      }))
-    .pipe(gulp.dest(prod + '/scripts/'))
+  .pipe(concat('script.min.js'))
+  .pipe(uglify().on('error', function(e){
+    console.log(e);
+  }))
+  .pipe(gulp.dest(prod + '/scripts/'))
 });
 
 gulp.task('default', function () {
